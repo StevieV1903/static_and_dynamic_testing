33a3ead84c3e45a67a2ce9eab46c3a760700457b
Steve Vance <steve.vance@hotmail.co.uk>
2019-12-29 14:56:36 +0000

    homework solution static testing added

---
+++ Homework_solution.rb
@@ -0,0 +1,41 @@
+### Testing task 1:
+
+# Carry out static testing on the code below.
+# Comment on any errors that you see below.
+# ```ruby
+
+class CardGame
+
+
+  def checkforAce(card)
+    # def check_for_ace( card )
+    if card.value = 1
+      #if card.value == 1
+      return true
+    else
+      return false
+    end
+  end
+
+  dif highest_card(card1 card2)
+  #def highest_card( card1, card2 )
+  if card1.value > card2.value
+    return card
+    #return card1
+  else
+    return card2
+  end
+end
+end
+#The final end is not required
+
+def self.cards_total(cards)
+  total
+  #total = []
+  for card in cards
+    total += card.value
+    return "You have a total of" + total
+    #return "You have a total of #{total}"
+  end
+end
+```
